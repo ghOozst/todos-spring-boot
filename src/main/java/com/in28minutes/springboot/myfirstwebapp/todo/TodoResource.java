f85725e87cbfbe6d17da2a96496975e8cc6ea19f
ghoozst01 <ghoozst01@gmail.com>
2024-01-21 12:01:38 -0600

    created a path to delete the todos of a username

--- src/main/java/com/in28minutes/springboot/myfirstwebapp/todo/TodoResource.java
+++ src/main/java/com/in28minutes/springboot/myfirstwebapp/todo/TodoResource.java
@@ -2,7 +2,9 @@ package com.in28minutes.springboot.myfirstwebapp.todo;
 
 import java.util.List;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,5 +23,16 @@ public class TodoResource {
 	@GetMapping("/users/{username}/todos")
 	public List<Todo> retrieveTodos(@PathVariable String username) {
 		return todoService.findByUsername(username);
+	}
+	
+	@GetMapping("/users/{username}/todos/{id}")
+	public Todo retrieveTodo(@PathVariable String username, @PathVariable int id) {
+		return todoService.findById(id);
+	}
+
+	@DeleteMapping("/users/{username}/todos/{id}")
+	public ResponseEntity<Void> deleteTodo(@PathVariable String username, @PathVariable int id) {
+		todoService.deleteById(id);
+		return ResponseEntity.noContent().build();
 	}
 }
