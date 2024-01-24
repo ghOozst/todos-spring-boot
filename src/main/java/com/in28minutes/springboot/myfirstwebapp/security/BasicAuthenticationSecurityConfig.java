f41657231e6dfc9690899361c05869cdc0a9cb71
ghoozst01 <ghoozst01@gmail.com>
2024-01-23 19:58:28 -0600

    Configured the spring security and added a route for authentication

---
+++ src/main/java/com/in28minutes/springboot/myfirstwebapp/security/BasicAuthenticationSecurityConfig.java
@@ -0,0 +1,33 @@
+package com.in28minutes.springboot.myfirstwebapp.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@Configuration
+public class BasicAuthenticationSecurityConfig {
+	@Bean 
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		
+		//Making necessary the authentication for all resquests
+		return 
+				http.authorizeHttpRequests(
+						auth -> 
+						auth
+						.anyRequest()
+						.authenticated()
+					)
+				.httpBasic(Customizer.withDefaults())
+				.sessionManagement( 
+						session -> session.sessionCreationPolicy
+						(SessionCreationPolicy.STATELESS)
+						)
+				.csrf().disable()
+				.build();
+	}
+}
--- src/main/java/com/in28minutes/springboot/myfirstwebapp/security/SpringSecurityConfiguration.java
+++ src/main/java/com/in28minutes/springboot/myfirstwebapp/security/SpringSecurityConfiguration.java
@@ -13,42 +13,42 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
-@Configuration
+//@Configuration
 public class SpringSecurityConfiguration {
-	@Bean
-	public InMemoryUserDetailsManager createUseerDetailsManager() {
-		
-		UserDetails userDetails1 = createNewUser("in28minutes", "dummy");
-		UserDetails userDetails2 = createNewUser("rafa", "a");
-		
-		return new InMemoryUserDetailsManager(userDetails1, userDetails2);
-	}
-
-	private UserDetails createNewUser(String username, String password) {
-		Function<String, String> passwordEncoder =input -> passwordEncoder().encode(input);
-		UserDetails userDetails = User.builder()
-								.passwordEncoder(passwordEncoder)
-								.username(username)
-								.password(password)
-								.roles("USER", "ADMIN")
-								.build();
-		return userDetails;
-	}
-	
-	@Bean
-	public PasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder();
-	}
-	
-	@Bean
-	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+//	@Bean
+//	public InMemoryUserDetailsManager createUseerDetailsManager() {
+//		
+//		UserDetails userDetails1 = createNewUser("in28minutes", "dummy");
+//		UserDetails userDetails2 = createNewUser("rafa", "a");
+//		
+//		return new InMemoryUserDetailsManager(userDetails1, userDetails2);
+//	}
+//
+//	private UserDetails createNewUser(String username, String password) {
+//		Function<String, String> passwordEncoder =input -> passwordEncoder().encode(input);
+//		UserDetails userDetails = User.builder()
+//								.passwordEncoder(passwordEncoder)
+//								.username(username)
+//								.password(password)
+//								.roles("USER", "ADMIN")
+//								.build();
+//		return userDetails;
+//	}
+//	
+//	@Bean
+//	public PasswordEncoder passwordEncoder() {
+//		return new BCryptPasswordEncoder();
+//	}
+//	
+//	@Bean
+//	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
 //		http.authorizeHttpRequests(
 //					auth -> auth.anyRequest().authenticated()
 //				);
-		
-		http.httpBasic(withDefaults());
-		http.csrf().disable();
-		http.headers().frameOptions().disable();
-		return http.build();
-	}
+//		
+//		http.httpBasic(withDefaults());
+//		http.csrf().disable();
+//		http.headers().frameOptions().disable();
+//		return http.build();
+//	}
 }
